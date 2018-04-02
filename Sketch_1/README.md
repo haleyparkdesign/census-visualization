4d960cbf7ba8a146ff2e524df076cba55429062d
hpark <hpark11@wellesley.edu>
2018-04-02 01:29:41 -0400

    Place blobs to look like U.S.

--- Sketch_1/README.md
+++ Sketch_1/README.md
@@ -1,6 +1,6 @@
+Temporary url: http://design-osaur.net/Sketch_1/
+
+
 Data source: https://factfinder.census.gov/faces/tableservices/jsf/pages/productview.xhtml?pid=ACS_15_5YR_S1501
 
 Based on this sketch: ![sketch 1](../hand_sketches/01.JPG "Sketch 1")
-
-Temporary url: http://design-osaur.net/Sketch_1/
-(planning to get a url once we have a team name)
--- Sketch_1/script.js
+++ Sketch_1/script.js
@@ -6,32 +6,34 @@ var margin = {
     l: 25
 };
 
+var statesByGeo = [
+    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "ME",
+    " ", " ", " ", " ", " ", " ", "WI", " ", " ", " ", "VT", "NH",
+    "", "WA", "ID", "MT", "ND", "MN", "IL", "MI", "", "NY", "MA", "",
+    "", "OR", "NV", "WY", "SD", "IA", "IN", "OH", "PA", "NJ", "CT", "RI",
+    " ", "CA", "UT", "CO", "NE", "MO", "KY", "WV", "VA", "MD", "DE", "",
+    "", "", "AZ", "NM", "KS", "AR", "TN", "NC", "SC", "DC", "", "",
+    "", "", "", "", "OK", "LA", "MS", "AL", "GA", "", "", "",
+    "HI", "AK", "", "", "TX", "", "", "", "", "FL", "", "PR"];
+
 var width = d3.select('#plot1').node().clientWidth - margin.r - margin.l,
     height = d3.select('#plot1').node().clientHeight - margin.t - margin.b;
 
 var plot1 = d3.select('#plot1')
-    .append('svg')
+
+var plot1_svg = plot1.append('svg')
     .attr('width', width + margin.r + margin.l)
     .attr('height', height + margin.t + margin.b);
 
-// function to draw the map
-var path = d3.geoPath();
-
-// prepare map (array) of data values
-var populationPerState = d3.map();
 
 // queue data files, parse them and use them
 var queue = d3.queue()
     .defer(d3.csv, "data/data.csv", parseData)
     .await(dataloaded);
 
-function dataloaded(err, data, map) {
-    console.log(data);
-    console.log(map);
-    console.log(populationPerState);
-
+function dataloaded(err, data) {
     // Bind the data to the SVG and create one path per GeoJSON feature
-    var node = plot1.selectAll(".state")
+    var node = plot1_svg.selectAll(".state")
         .data(data)
         .enter()
         .append("g")
@@ -61,7 +63,7 @@ function dataloaded(err, data, map) {
 
             data.forEach(function (e) {
                 if (mapID === e.id) {
-                    r = (e.total) * 3
+                    r = e.radius;
                 }
             });
 
@@ -71,50 +73,61 @@ function dataloaded(err, data, map) {
         .attr("stroke", "#fff")
         .attr("stroke-width", 1)
 
-
     node.append("text")
         .attr("text-anchor", "middle")
         .text(function (d) {
-            console.log(d);
-            for (i = 0; i < us_states.length; i++) {
-                if (us_states[i].name == d.state.toUpperCase()) {
-                    return us_states[i].abbreviation;
-                }
-            }
-        });
+            return d.stateAbbr
+        })
+        .style("transform", "translate(0px, 4px)");
 
-    var tooltip = d3.select('#plot1')
+    var tooltip = plot1
         .append("div")
         .attr("class", "tooltip")
 
     var force = d3.forceSimulation(data)
-        .force("charge", d3.forceManyBody().strength(-100))
-        .force("x", d3.forceX())
-        .force("y", d3.forceY())
-        .force('collision', d3.forceCollide().radius(function (d) {
-            console.log(d);
-            console.log(d.radius);
-            return d.radius
-        }))
         .on("tick", ticked);
 
     function ticked(e) {
         node.attr("transform", function (d) {
-            return "translate(" + [d.x + (width / 2), d.y + ((height) / 2)] + ")";
+            return "translate(" + [placeState(d.stateAbbr)[0] + (width / 7), placeState(d.stateAbbr)[1] + ((height) / 9)] + ")";
         });
-    }
+    };
 }
 
 function parseData(d) {
     var id = d.Id.split("US")[1];
     var total = d["Percent; Estimate; Population 18 to 24 years - Less than high school graduate"]
-    console.log(total);
-    var radius = total * 3
+    var radius = total * 2;
+    var state = d.Geography;
+    var stateAbbr;
+    for (i = 0; i < us_states.length; i++) {
+        if (us_states[i].name == state.toUpperCase()) {
+            stateAbbr = us_states[i].abbreviation;
+        }
+    }
 
     return {
         id: +id,
-        state: d.Geography,
+        state: state,
         total: +total,
-        radius: +radius
+        radius: +radius,
+        stateAbbr: stateAbbr
     }
+}
+
+function placeState(state) {
+    var x = 0;
+    var y = 0;
+    var factor = 60; //distribution factor
+
+    for (i = 0; i < statesByGeo.length; i++) {
+        if (statesByGeo[i] == state) {
+            x = i % 12;
+            y = Math.round(i / 12) - 1;
+        }
+    }
+
+    x = x * factor;
+    y = y * factor;
+    return [x, y]
 }
