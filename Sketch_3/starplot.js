8376d126dcd7e70a96b0c4ec49573f98de685e31
noahmcd <36311633+noahmcd@users.noreply.github.com>
2018-04-05 16:52:29 -0400

    Update starplot.js

--- Sketch_3/starplot.js
+++ Sketch_3/starplot.js
@@ -1,5 +1,5 @@
 var margin = {top: 10, bottom: 10, left: 10, right: 10};
-var width =  780;//d3.select("#plot3").node().clientWidth - margin.left - margin.right;
+var width =  d3.select("#plot3").node().clientWidth - margin.left - margin.right;
 var height =  d3.select("#plot3").node().clientHeight - margin.top - margin.bottom;
 var radius = height*2/5;
 
