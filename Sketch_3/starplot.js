c3eb96f335a3962527fd29af8c9545efe435f8d6
noahmcd <36311633+noahmcd@users.noreply.github.com>
2018-04-11 17:34:34 -0400

    Update starplot.js

--- Sketch_3/starplot.js
+++ Sketch_3/starplot.js
@@ -10,7 +10,7 @@ var plot = d3.select("#plot3")
     .attr('height', height + margin.top + margin.bottom);
 
 var queue = d3.queue()
-    .defer(d3.csv, "income_education_condensed.csv", parseData)
+    .defer(d3.csv, "Sketch_3/income_education_condensed.csv", parseData)
     .await(dataloaded);
 
 var colors = ["#3961a0", "#a03887", "#7b38a0", "#38a057", "#98dd6a", "#dddd69", "#d89950", "#d85d50", "#1ed278", "#ff849f"]
